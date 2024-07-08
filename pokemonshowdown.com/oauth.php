bb06a500eb13ca939fac44d765c76a0a3bd30984
anastarawneh <anastarawneh2001@icloud.com>
2024-07-08 20:17:15 +0300

    Remove cookie check in oauth.php Reaching this page probably means auth was already attempted, no need to prevent anything

--- pokemonshowdown.com/oauth.php
+++ pokemonshowdown.com/oauth.php
@@ -8,39 +8,28 @@ $pageTitle = "OAuth";
 
 includeHeader();
 
-if (!isset($_COOKIE['sid'])) {
-	try {
-		$assertion = $_GET['assertion'];
-		$token = $_GET['token'];
-		$username = explode(',', $assertion)[1];
-		header('Set-Cookie: sid='.$username.',,'.$token.'; Domain=fakemons.anastarawneh.com; Max-Age=604800; Path=/; Secure; SameSite=None');
-		header('Set-Cookie: assertion='.urlencode($assertion).'; Domain=fakemons.anastarawneh.com; Max-Age=604800; Path=/; Secure; SameSite=None', false);
-		?>
-			<div class="main">
-				<h1>You have been successfully authenticated.</h1>
-
-				<p>You may close this page now.</p>
-			</div>
-		<?php
-	} catch (Exception $e) {
-		?>
-			<div class="main">
-				<h1>An error occured.</h1>
-
-				<p>Try contacting Anas about this.</p>
-			</div>
-		<?php
-	}
-} else {
+try {
+	$assertion = $_GET['assertion'];
+	$token = $_GET['token'];
+	$username = explode(',', $assertion)[1];
+	header('Set-Cookie: sid='.$username.',,'.$token.'; Domain=fakemons.anastarawneh.com; Max-Age=604800; Path=/; Secure; SameSite=None');
+	header('Set-Cookie: assertion='.urlencode($assertion).'; Domain=fakemons.anastarawneh.com; Max-Age=604800; Path=/; Secure; SameSite=None', false);
 	?>
-			<div class="main">
-				<h1>You are already authenticated.</h1>
+		<div class="main">
+			<h1>You have been successfully authenticated.</h1>
 
-				<p>You may close this page now.</p>
-			</div>
+			<p>You may close this page now.</p>
+		</div>
+	<?php
+} catch (Exception $e) {
+	?>
+		<div class="main">
+			<h1>An error occured.</h1>
+
+			<p>Try contacting Anas about this.</p>
+		</div>
 	<?php
 }
-
 
 includeFooter();
 
